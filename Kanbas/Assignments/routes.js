13231f8f27a33edf9c5628f67d501031edc42b62
Yashb0299 <yashb0299@gmail.com>
2024-08-03 03:11:21 -0400

    Update routes.js

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,4 +1,4 @@
-import db from "../DataBase/index.js";
+import db from "../Database/index.js";
 
 export default function AssignmentRoutes(app) {
     app.put("/api/assignments/:aid", (req, res) => {
